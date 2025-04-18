96c5c61fbd0ac4c44102231cf47eca01cc0122db
Sapozhnik <yasapozhnik@gmail.com>
2025-04-18 21:09:39 +0900

    ww

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
 
   sassOptions: {
     includePaths: ['./src'],
-    additionalData: `@import './src/styles/variables'; @import './src/styles/mixins';`
+    additionalData: `@use './src/styles/variables'; @use './src/styles/mixins';`
   },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
